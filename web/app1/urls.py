503ecfe0a4c873ddd410202afb67b86d6f5296e2
pooriya adib <adibradp@gmail.com>
2024-03-01 10:16:20 +0330

    this is for django student

--- web/app1/urls.py
+++ web/app1/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
-
+from . import views
 urlpatterns = [
-    path('', views)
+    path('', views.index, name='index'),
+    path('data/', views.data, name='data'),
+    path('datatable/', views.datatable, name='data'),
 ]
--- web/app1/views.py
+++ web/app1/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
-
+from . import models
 # Create your views here.
+def index(request):
+     return render(request=request, template_name='index.html',context={})
+
+
+def data(request):
+     user=models.User(Email="adibradp@gmail.com",Password="poo123456789")
+     user.save()
+     return render(request=request, template_name='data.html',context={})
+
+
+def datatable(request):
+     alldata=models.User.objects.all()
+     return render(request=request, template_name='datatable.html',context={'alldata':alldata})
